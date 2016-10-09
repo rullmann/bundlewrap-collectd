e5797e25d0310b248ab1c50be8b62dbc1d04f635
Rico Ullmann <rico@erinnerungsfragmente.de>
2016-10-09 11:50:43 +0200

    firewalld rules should only be added if node is a collectd server

--- items.py
+++ items.py
@@ -48,6 +48,24 @@ if node.metadata.get('collectd', {}).get('write_rrd', True):
         ],
     }
 
+if node.metadata.get('collectd', {}).get('client'):
+    files['/etc/collectd.d/client.conf'] = {
+        'source': "client.conf",
+        'owner': "root",
+        'group': "root",
+        'mode': "0640",
+        'content_type': "mako",
+        'context': {
+            'client': node.metadata.get('collectd', {}).get('client', {}),
+        },
+        'needs': [
+            "pkg_yum:collectd",
+        ],
+        'triggers': [
+            "svc_systemd:collectd:restart",
+        ],
+    }
+
 if node.metadata.get('collectd', {}).get('server'):
     files['/etc/collectd.d/server.conf'] = {
         'source': "server.conf",
@@ -79,58 +97,13 @@ if node.metadata.get('collectd', {}).get('server'):
         ],
     }
 
-if node.metadata.get('collectd', {}).get('client'):
-    files['/etc/collectd.d/client.conf'] = {
-        'source': "client.conf",
-        'owner': "root",
-        'group': "root",
-        'mode': "0640",
-        'content_type': "mako",
-        'context': {
-            'client': node.metadata.get('collectd', {}).get('client', {}),
-        },
-        'needs': [
-            "pkg_yum:collectd",
-        ],
-        'triggers': [
-            "svc_systemd:collectd:restart",
-        ],
-    }
-
-if node.has_bundle("firewalld"):
-    port = node.metadata.get('collectd', {}).get('server', {}).get('port', "25826")
-    if node.metadata.get('collectd', {}).get('server', {}).get('firewalld_permitted_zone'):
-        zone = node.metadata.get('collectd', {}).get('server', {}).get('firewalld_permitted_zone')
-        actions['firewalld_add_collectd_zone_{}'.format(zone)] = {
-            'command': "firewall-cmd --permanent --zone={} --add-port={}/udp".format(zone, port),
-            'unless': "firewall-cmd --zone={} --list-ports | grep {}/udp".format(zone, port),
-            'cascade_skip': False,
-            'needs': [
-                "pkg_yum:firewalld",
-            ],
-            'triggers': [
-                "action:firewalld_reload",
-            ],
-        }
-    elif node.metadata.get('firewalld', {}).get('default_zone'):
-        default_zone = node.metadata.get('firewalld', {}).get('default_zone')
-        actions['firewalld_add_collectd_zone_{}'.format(default_zone)] = {
-            'command': "firewall-cmd --permanent --zone={} --add-port={}/udp".format(default_zone, port),
-            'unless': "firewall-cmd --zone={} --list-ports | grep {}/udp".format(default_zone, port),
-            'cascade_skip': False,
-            'needs': [
-                "pkg_yum:firewalld",
-            ],
-            'triggers': [
-                "action:firewalld_reload",
-            ],
-        }
-    elif node.metadata.get('firewalld', {}).get('custom_zones', False):
-        for interface in node.metadata['interfaces']:
-            custom_zone = node.metadata.get('interfaces', {}).get(interface).get('firewalld_zone')
-            actions['firewalld_add_collectd_zone_{}'.format(custom_zone)] = {
-                'command': "firewall-cmd --permanent --zone={} --add-port={}/udp".format(custom_zone, port),
-                'unless': "firewall-cmd --zone={} --list-ports | grep {}/udp".format(custom_zone, port),
+    if node.has_bundle("firewalld"):
+        port = node.metadata.get('collectd', {}).get('server', {}).get('port', "25826")
+        if node.metadata.get('collectd', {}).get('server', {}).get('firewalld_permitted_zone'):
+            zone = node.metadata.get('collectd', {}).get('server', {}).get('firewalld_permitted_zone')
+            actions['firewalld_add_collectd_zone_{}'.format(zone)] = {
+                'command': "firewall-cmd --permanent --zone={} --add-port={}/udp".format(zone, port),
+                'unless': "firewall-cmd --zone={} --list-ports | grep {}/udp".format(zone, port),
                 'cascade_skip': False,
                 'needs': [
                     "pkg_yum:firewalld",
@@ -139,15 +112,42 @@ if node.has_bundle("firewalld"):
                     "action:firewalld_reload",
                 ],
             }
-    else:
-        actions['firewalld_add_https'] = {
-            'command': "firewall-cmd --permanent --add-port={}/udp".format(port),
-            'unless': "firewall-cmd --list-ports | grep {}/udp".format(port),
-            'cascade_skip': False,
-            'needs': [
-                "pkg_yum:firewalld",
-            ],
-            'triggers': [
-                "action:firewalld_reload",
-            ],
-        }
+        elif node.metadata.get('firewalld', {}).get('default_zone'):
+            default_zone = node.metadata.get('firewalld', {}).get('default_zone')
+            actions['firewalld_add_collectd_zone_{}'.format(default_zone)] = {
+                'command': "firewall-cmd --permanent --zone={} --add-port={}/udp".format(default_zone, port),
+                'unless': "firewall-cmd --zone={} --list-ports | grep {}/udp".format(default_zone, port),
+                'cascade_skip': False,
+                'needs': [
+                    "pkg_yum:firewalld",
+                ],
+                'triggers': [
+                    "action:firewalld_reload",
+                ],
+            }
+        elif node.metadata.get('firewalld', {}).get('custom_zones', False):
+            for interface in node.metadata['interfaces']:
+                custom_zone = node.metadata.get('interfaces', {}).get(interface).get('firewalld_zone')
+                actions['firewalld_add_collectd_zone_{}'.format(custom_zone)] = {
+                    'command': "firewall-cmd --permanent --zone={} --add-port={}/udp".format(custom_zone, port),
+                    'unless': "firewall-cmd --zone={} --list-ports | grep {}/udp".format(custom_zone, port),
+                    'cascade_skip': False,
+                    'needs': [
+                        "pkg_yum:firewalld",
+                    ],
+                    'triggers': [
+                        "action:firewalld_reload",
+                    ],
+                }
+        else:
+            actions['firewalld_add_https'] = {
+                'command': "firewall-cmd --permanent --add-port={}/udp".format(port),
+                'unless': "firewall-cmd --list-ports | grep {}/udp".format(port),
+                'cascade_skip': False,
+                'needs': [
+                    "pkg_yum:firewalld",
+                ],
+                'triggers': [
+                    "action:firewalld_reload",
+                ],
+            }
