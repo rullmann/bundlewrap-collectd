e179a888959b9d81a25f9d9854a7b904173302e5
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-09-30 23:04:58 +0200

    streamline needs and triggers

--- items.py
+++ items.py
@@ -9,9 +9,7 @@ pkg_dnf = {
 
 svc_systemd = {
     'collectd': {
-        'needs': [
-            'pkg_dnf:collectd',
-        ],
+        'needs': ['pkg_dnf:collectd'],
     },
 }
 
@@ -23,13 +21,8 @@ files = {
         'context': {
             'collectd': node.metadata.get('collectd', {}),
         },
-        'needs': [
-            'pkg_dnf:collectd',
-            'pkg_dnf:rrdtool',
-        ],
-        'triggers': [
-            'svc_systemd:collectd:restart',
-        ],
+        'needs': ['pkg_dnf:collectd', 'pkg_dnf:rrdtool'],
+        'triggers': ['svc_systemd:collectd:restart'],
     },
 }
 
@@ -38,15 +31,11 @@ actions = {}
 directories = {
     '/etc/collectd.d/plugins': {
         'mode': '0755',
-        'needs': [
-            'pkg_dnf:collectd',
-        ],
+        'needs': ['pkg_dnf:collectd'],
     },
     '/etc/collectd.d/types': {
         'mode': '0755',
-        'needs': [
-            'pkg_dnf:collectd',
-        ],
+        'needs': ['pkg_dnf:collectd'],
     },
 }
 
@@ -54,9 +43,7 @@ git_deploy = {}
 
 if node.metadata.get('collectd', {}).get('write_rrd', True):
     pkg_dnf['collectd-rrdtool'] = {
-        'triggers': [
-            'svc_systemd:collectd:restart',
-        ],
+        'triggers': ['svc_systemd:collectd:restart'],
     }
 
 if node.metadata.get('collectd', {}).get('client'):
@@ -67,12 +54,8 @@ if node.metadata.get('collectd', {}).get('client'):
         'context': {
             'client': node.metadata.get('collectd', {}).get('client', {}),
         },
-        'needs': [
-            'pkg_dnf:collectd',
-        ],
-        'triggers': [
-            'svc_systemd:collectd:restart',
-        ],
+        'needs': ['pkg_dnf:collectd'],
+        'triggers': ['svc_systemd:collectd:restart'],
     }
 
 if node.metadata.get('collectd', {}).get('server'):
@@ -83,23 +66,15 @@ if node.metadata.get('collectd', {}).get('server'):
         'context': {
             'server': node.metadata.get('collectd', {}).get('server', {}),
         },
-        'needs': [
-            'pkg_dnf:collectd',
-        ],
-        'triggers': [
-            'svc_systemd:collectd:restart',
-        ],
+        'needs': ['pkg_dnf:collectd'],
+        'triggers': ['svc_systemd:collectd:restart'],
     }
 
     files['/etc/collectd.d/collectd.auth'] = {
         'source': 'server_auth/{}.auth'.format(node.name),
         'mode': '0600',
-        'needs': [
-            'pkg_dnf:collectd',
-        ],
-        'triggers': [
-            'svc_systemd:collectd:restart',
-        ],
+        'needs': ['pkg_dnf:collectd'],
+        'triggers': ['svc_systemd:collectd:restart'],
     }
 
     if node.has_bundle('firewalld'):
@@ -110,12 +85,8 @@ if node.metadata.get('collectd', {}).get('server'):
                 'command': 'firewall-cmd --permanent --zone={} --add-port={}/udp'.format(zone, port),
                 'unless': 'firewall-cmd --zone={} --list-ports | grep {}/udp'.format(zone, port),
                 'cascade_skip': False,
-                'needs': [
-                    'pkg_dnf:firewalld',
-                ],
-                'triggers': [
-                    'action:firewalld_reload',
-                ],
+                'needs': ['pkg_dnf:firewalld'],
+                'triggers': ['action:firewalld_reload'],
             }
         elif node.metadata.get('firewalld', {}).get('default_zone'):
             default_zone = node.metadata.get('firewalld', {}).get('default_zone')
@@ -123,12 +94,8 @@ if node.metadata.get('collectd', {}).get('server'):
                 'command': 'firewall-cmd --permanent --zone={} --add-port={}/udp'.format(default_zone, port),
                 'unless': 'firewall-cmd --zone={} --list-ports | grep {}/udp'.format(default_zone, port),
                 'cascade_skip': False,
-                'needs': [
-                    'pkg_dnf:firewalld',
-                ],
-                'triggers': [
-                    'action:firewalld_reload',
-                ],
+                'needs': ['pkg_dnf:firewalld'],
+                'triggers': ['action:firewalld_reload'],
             }
         elif node.metadata.get('firewalld', {}).get('custom_zones', False):
             for interface in node.metadata['interfaces']:
@@ -137,24 +104,16 @@ if node.metadata.get('collectd', {}).get('server'):
                     'command': 'firewall-cmd --permanent --zone={} --add-port={}/udp'.format(custom_zone, port),
                     'unless': 'firewall-cmd --zone={} --list-ports | grep {}/udp'.format(custom_zone, port),
                     'cascade_skip': False,
-                    'needs': [
-                        'pkg_dnf:firewalld',
-                    ],
-                    'triggers': [
-                        'action:firewalld_reload',
-                    ],
+                    'needs': ['pkg_dnf:firewalld'],
+                    'triggers': ['action:firewalld_reload'],
                 }
         else:
             actions['firewalld_add_https'] = {
                 'command': 'firewall-cmd --permanent --add-port={}/udp'.format(port),
                 'unless': 'firewall-cmd --list-ports | grep {}/udp'.format(port),
                 'cascade_skip': False,
-                'needs': [
-                    'pkg_dnf:firewalld',
-                ],
-                'triggers': [
-                    'action:firewalld_reload',
-                ],
+                'needs': ['pkg_dnf:firewalld'],
+                'triggers': ['action:firewalld_reload'],
             }
 
 if node.metadata.get('collectd', {}).get('cgp', {}):
@@ -174,9 +133,7 @@ if node.metadata.get('collectd', {}).get('cgp', {}):
     files['{}/conf/config.local.php'.format(cgp_install_path)] = {
         'source': 'cgp_config',
         'mode': '0644',
-        'needs': [
-            'git_deploy:{}'.format(cgp_install_path)
-        ],
+        'needs': ['git_deploy:{}'.format(cgp_install_path)],
     }
 
 if node.has_bundle('monit'):
@@ -187,7 +144,5 @@ if node.has_bundle('monit'):
         'context': {
             'server': node.metadata.get('collectd', {}).get('server', {}),
         },
-        'triggers': [
-            'svc_systemd:monit:restart',
-        ],
+        'triggers': ['svc_systemd:monit:restart'],
     }
